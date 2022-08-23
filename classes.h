a521eb816d463cb59c422f1030f850bdfa9a9f7a
FilipHa5 <filipha5@wp.pl>
2022-08-23 23:45:14 +0200

    Added Container class

--- classes.h
+++ classes.h
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <string>
 
-class Towar {
+using namespace std;
+
+class Cargo {
 protected: 
     string name;
     string type;
@@ -16,10 +18,45 @@ public:
         cout<<"Total mass: "<<total_mass<<endl;
     }
     
-Towar(string n, string t, int q, float m){
+Cargo(string n, string t, int q, float m){
     name = n;
     type = t;
     quantity = q;
     total_mass = m;
+}
+};
+
+class Container : public Cargo {
+protected:
+    string number;
+    float length;
+    float breadth;
+    float height;
+    float unit_mass;
+
+public:
+    void info(){
+        cout<<"Name: "<<name<<endl;
+        cout<<"Type: "<<type<<endl;
+        cout<<"Number: "<<number;
+        cout<<"Length: "<<length;
+        cout<<"Breadth: "<<breadth;
+        cout<<"Heigth: "<<height;
+        cout<<"Quantity: "<<quantity<<endl;
+        cout<<"Unit mass: "<<unit_mass;
+        cout<<"Total mass: "<<total_mass<<endl;
+
+    }
+Container(string n, string t, string nr, float l, float b, float h, float um)
+:Cargo(n, t, q){
+    name = n;
+    type = t;
+    quantity = q;
+    number = nr;
+    length = l;
+    breadth = b;
+    height = h;
+    unit_mass = um;
+    total_mass = um*quantity;
 }
 };
