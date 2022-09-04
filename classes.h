ee72572c039d8f7b7bf834cbfb5cc4a386324208
FilipHa5 <filipha5@wp.pl>
2022-09-04 15:05:28 +0200

    Fixed bugs

--- classes.h
+++ classes.h
@@ -2,7 +2,16 @@
 #include <string>
 #include <vector>
 
-class Queue : public Cargo {
+class Cargo {
+public:
+    virtual void info() = 0;/*
+    virtual void getVolume() = 0;
+    virtual void getDensity() = 0;
+    virtual void receipt() = 0;
+    virtual void relase() = 0;*/
+};
+
+class Queue : public Cargo{
 private:
     std::vector<Cargo*> queue;
 public:
@@ -12,19 +21,10 @@ public:
     int queueCount(){
         return queue.size();
     }
-    virtual void queueCargo(Cargo*) = 0{
-        queue.push_back(Cargo*);
+    virtual void queueCargo(Cargo* ptr){
+        queue.push_back(ptr);
     }
 
-};
-
-class Cargo {
-public:
-    vitrual void info() = 0;
-    virtual void getVolume() = 0;
-    virtual void getDensity() = 0;
-    virtual void receipt() = 0;
-    virtual void relase() = 0;
 };
 
 class Container : public Cargo {
@@ -49,16 +49,16 @@ public:
         this->unit_mass = um;
         this->quantity = q;
     }
-    virutal void info(){
-        std::cout<<"Name: "<<name<<endl;
-        std::cout<<"Type: "<<type<<endl;
-        std::cout<<"Number: "<<number;
-        std::cout<<"Length: "<<length;
-        std::cout<<"Breadth: "<<breadth;
-        std::cout<<"Heigth: "<<height;
-        std::cout<<"Unit mass: "<<unit_mass;
-        std::cout<<"Quantity: "<<quantity<<endl;
-        std::cout<<"Total mass: "<<unit_mass*quantity<<endl;
+    virtual void info(){
+        std::cout<<"Name: "<<name<<std::endl;
+        std::cout<<"Type: "<<type<<std::endl;
+        std::cout<<"Number: "<<number<<std::endl;
+        std::cout<<"Length: "<<length<<std::endl;
+        std::cout<<"Breadth: "<<breadth<<std::endl;
+        std::cout<<"Heigth: "<<height<<std::endl;
+        std::cout<<"Unit mass: "<<unit_mass<<std::endl;
+        std::cout<<"Quantity: "<<quantity<<std::endl;
+        std::cout<<"Total mass: "<<unit_mass*quantity<<std::endl;
 
     }
 };
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "classes.h"
 
 int main(){
-    Cargo* ptr = new  Container;
+    Cargo* ptr = new  Container("Moj kontener", "Zwykly", "AS123", 10.3, 3.2, 2.2, 1095, 5);
+    ptr->info();
+    system("PAUSE");
     return 0;
 }
